1f280120624895951d9399b5bc851be8e5d46b1f
Hunter Matthews <hunter.matthews@utdallas.edu>
2022-11-04 18:27:13 -0500

    Update Main.hs

--- Elementary Number Theory/Final Project/Main.hs
+++ Elementary Number Theory/Final Project/Main.hs
@@ -7,3 +7,22 @@
 -- Stability   : experimental
 -- Portability : portable
 -- 
+import Data.List
+import System.IO
+import Primes
+
+findPairs :: Integer -> IO ()
+findPairs n = do
+  if n > 1
+  then do
+    let p = (3 * 2^(n-1)) - 1
+    let q = (3 * 2^n) - 1
+    let r = (9 * (2^(2 * n - 1)) - 1)
+    if isPrime(p) == True && isPrime(q) == True && isPrime(r) == True
+    then do
+      print(n, (2^n) * p * q, (2^n) * r)
+      findPairs(n-1)
+    else
+      findPairs(n-1)
+  else
+    print("DONE")
