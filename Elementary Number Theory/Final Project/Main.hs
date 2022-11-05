b81e67f3f06fe9d65cc7747b317a41d87b07006f
Hunter Matthews <hunter.matthews@utdallas.edu>
2022-11-04 20:14:56 -0500

    Update Main.hs

--- Elementary Number Theory/Final Project/Main.hs
+++ Elementary Number Theory/Final Project/Main.hs
@@ -1,15 +1,11 @@
--- |
--- Module      : Main
--- Copyright   : Hunter Matthews
--- License     : BSD3
--- 
--- Maintainer  : Hunter Matthews
--- Stability   : experimental
--- Portability : portable
--- 
-import Data.List
-import System.IO
+--------------------------------------------------------------------------------
+-- author: Hunter Matthews
+-- Unlicense (see http://unlicense.org/)
+--------------------------------------------------------------------------------
 import Primes
+import System.Environment
+import System.IO
+import System.Random
 
 findPairs :: Integer -> IO ()
 findPairs n = do
@@ -18,11 +14,12 @@ findPairs n = do
     let p = (3 * 2^(n-1)) - 1
     let q = (3 * 2^n) - 1
     let r = (9 * (2^(2 * n - 1)) - 1)
-    if isPrime(p) == True && isPrime(q) == True && isPrime(r) == True
+    seed <- newStdGen
+    if isPrime seed p == True && isPrime seed q == True && isPrime seed r == True 
     then do
       print(n, (2^n) * p * q, (2^n) * r)
       findPairs(n-1)
     else
       findPairs(n-1)
   else
-    print("DONE")
+    return ()
