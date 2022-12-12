5de53653b8b4cdf940bfc1ca62c38b9522188efc
Hunter Matthews <hunter.matthews@utdallas.edu>
2022-12-11 20:20:08 -0600

    Update Main.hs

--- Elementary Number Theory/Final Project/Main.hs
+++ Elementary Number Theory/Final Project/Main.hs
@@ -2,24 +2,34 @@
 -- author: Hunter Matthews
 -- Unlicense (see http://unlicense.org/)
 --------------------------------------------------------------------------------
-import Primes
-import System.Environment
+{-# OPTIONS_GHC -fobject-code -O #-}
+
+module Main where
+
+import Math.NumberTheory.Primes.Testing
 import System.IO
-import System.Random
+import Data.Time
 
 findPairs :: Integer -> IO ()
 findPairs n = do
-  if n > 1
-  then do
-    let p = (3 * 2^(n-1)) - 1
-    let q = (3 * 2^n) - 1
-    let r = (9 * (2^(2 * n - 1)) - 1)
-    seed <- newStdGen
-    if isPrime seed p == True && isPrime seed q == True && isPrime seed r == True 
+  if n > 230000
     then do
-      print(n, (2^n) * p * q, (2^n) * r)
-      findPairs(n-1)
-    else
-      findPairs(n-1)
+      let p = (3 * 2^(n-1)) - 1
+          q = (3 * 2^n) - 1
+          r = (9 * (2^(2 * n - 1))) - 1
+      if isPrime p == True && isPrime q == True && isPrime r == True
+        then do
+          print(n)
+          findPairs(n - 1)
+        else
+          findPairs(n - 1)
   else
     return ()
+
+main :: IO ()
+main = do
+  currentTime <- getCurrentTime
+  putStrLn ("Searching started @ " ++ show currentTime)
+  findPairs 240000
+  currentTime <- getCurrentTime
+  putStrLn ("Search completed @ " ++ show currentTime)
